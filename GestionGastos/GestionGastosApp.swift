78fe1e979b16c776e655bd444eb5db0782bf24c4
Tania <taniabg2023@gmail.com>
2025-02-11 22:55:55 +0100

    Initial Commit

---
+++ GestionGastos/GestionGastosApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestionGastosApp.swift
+//  GestionGastos
+//
+//  Created by Tania Bajo García on 11/2/25.
+//
+
+import SwiftUI
+
+@main
+struct GestionGastosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GestionGastosTests/GestionGastosTests.swift
@@ -0,0 +1,17 @@
+//
+//  GestionGastosTests.swift
+//  GestionGastosTests
+//
+//  Created by Tania Bajo García on 11/2/25.
+//
+
+import Testing
+@testable import GestionGastos
+
+struct GestionGastosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
